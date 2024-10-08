a1f104778b75b29af94e5a977758b2bcf1c421a0
Michael Fuller <33374714+michaeljfuller@users.noreply.github.com>
2024-10-08 22:49:41 +0100

    style: set background color

--- app/root.tsx
+++ app/root.tsx
@@ -7,7 +7,8 @@ import {
 } from "@remix-run/react";
 import type { LinksFunction } from "@remix-run/node";
 
-import "./tailwind.css";
+import "./styles/tailwind.css";
+import "./styles/root.module.scss";
 
 export const links: LinksFunction = () => [
   { rel: "preconnect", href: "https://fonts.googleapis.com" },
